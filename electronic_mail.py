5c24dd0312f06750ac8794fdbea057c2f73dbc72
resteve <resteve@zikzakmedia.com>
2014-09-08 09:57:30 +0200

    pyflakes

--- electronic_mail.py
+++ electronic_mail.py
@@ -1,7 +1,7 @@
 #This file is part electronic_mail_template module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-from trytond.model import ModelView, fields
+from trytond.model import ModelView
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval, Bool
 
