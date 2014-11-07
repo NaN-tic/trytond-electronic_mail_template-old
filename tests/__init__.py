e612c10ccc78a34073932fc686af14043e874a13
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:22:53 +0100

    Cleanup tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-#This file is part electronic_mail_template module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part electronic_mail_template module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from test_electronic_mail_template import suite
 
