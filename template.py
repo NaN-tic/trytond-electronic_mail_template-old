c9b28834072d0f62e871956ba910e5f7af504b03
Oscar Alvarez <oscar.alvarez.montero@gmail.com>
2015-07-11 09:23:44 -0500

    Fix missing styles on setup.py and removed safe_eval deprecation

--- template.py
+++ template.py
@@ -17,7 +17,6 @@ from email import Encoders, charset
 from genshi.template import TextTemplate
 from trytond import backend
 from trytond.model import ModelView, ModelSQL, fields
-from trytond.tools import safe_eval
 from trytond.transaction import Transaction
 from trytond.pyson import Eval
 from trytond.pool import Pool
@@ -158,7 +157,7 @@ class Template(ModelSQL, ModelView):
 
         assert record is not None, 'Record is undefined'
         template_context = cls.template_context(record)
-        return safe_eval(expression, template_context)
+        return eval(expression, template_context)
 
     @classmethod
     def _engine_genshi(cls, expression, record):
