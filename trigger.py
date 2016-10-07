d426a72eb432fd78aab7d1bd3932aaa968cd279b
Raimon Esteve <raimon@nan-tic.com>
2016-10-07 16:16:10 +0200

    FIX default trigger methods return None or id

--- trigger.py
+++ trigger.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
-# This file is part electronic_mail_template module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+#This file is part electronic_mail_template module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
 "Trigger Extension"
 from trytond.model import fields
 from trytond.transaction import Transaction
@@ -20,25 +20,24 @@ class Trigger:
 
     @staticmethod
     def default_model():
-        """If invoked from the email_template fill model
-        """
-        return Transaction().context.get('model', False)
+        Model = Pool().get('ir.model')
+
+        model = Transaction().context.get('model', None)
+        if model:
+            return model
 
     @staticmethod
     def default_action_model():
-        """If invoked from the email_template fill 
-        action model as email_template
-        """
         Model = Pool().get('ir.model')
 
         email_trigger = Transaction().context.get('email_template', False)
         if not email_trigger:
-            return False
+            return
 
-        model_ids = Model.search(
-            [('model', '=', 'electronic.mail.template')])
-        assert len(model_ids) == 1, 'Unexpected result for model search'
-        return model_ids[0].id
+        models = Model.search(
+            [('model', '=', 'electronic.mail.template')], limit=1)
+        if models:
+            return models[0].id
 
     @staticmethod
     def default_action_function():
@@ -46,4 +45,4 @@ class Trigger:
         action function as 'mail_from_trigger'
         """
         email_trigger = Transaction().context.get('email_template', False)
-        return email_trigger and 'mail_from_trigger' or False
+        return email_trigger and 'mail_from_trigger' or None
